40100bcdc1058d197888b562bb8168c724e53c87
Michał Wilczyński <michal@wilczynski.pro>
2020-02-03 23:12:32 +0100

    Using explicit serializers for both Utf8Json and MessagePack.

--- src/Outbox/Resonance.Outbox.Serialization.MessagePack/MessagePackSerializer.cs
+++ src/Outbox/Resonance.Outbox.Serialization.MessagePack/MessagePackSerializer.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using MessagePack;
+using MessagePack.Resolvers;
 
 namespace Resonance.Outbox.Serialization.MessagePack
 {
     public class MessagePackSerializer : IMessageSerializer
     {
+        private readonly MessagePackSerializerOptions _serializerOptions = 
+            MessagePackSerializerOptions.Standard
+                .WithCompression(MessagePackCompression.Lz4Block)
+                .WithAllowAssemblyVersionMismatch(true)
+                .WithResolver(DynamicContractlessObjectResolverAllowPrivate.Instance);
+
         public byte[] Serialize<TMessage>(TMessage message)
         {
-            return global::MessagePack.MessagePackSerializer.Serialize(message);
+            return global::MessagePack.MessagePackSerializer.Serialize(message, _serializerOptions);
         }
 
         public object Deserialize(Type messageType, byte[] message)
         {
-            return global::MessagePack.MessagePackSerializer.Deserialize(messageType, message);
+            return global::MessagePack.MessagePackSerializer.Deserialize(messageType, message, _serializerOptions);
         }
     }
 }
--- src/Outbox/Resonance.Outbox.Serialization.Utf8Json/Utf8JsonSerializer.cs
+++ src/Outbox/Resonance.Outbox.Serialization.Utf8Json/Utf8JsonSerializer.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using Utf8Json;
+using Utf8Json.Resolvers;
 
 namespace Resonance.Outbox.Serialization.Utf8Json
 {
     public class Utf8JsonSerializer : IMessageSerializer
     {
+        private IJsonFormatterResolver _resolver = StandardResolver.AllowPrivate;
+
         public byte[] Serialize<TMessage>(TMessage message)
         {
             //TODO: Compare with performance using MemoryStream && SerializeAsync
-            return JsonSerializer.Serialize(message);
+            return JsonSerializer.Serialize(message, _resolver);
         }
 
         public object Deserialize(Type messageType, byte[] message)
         {
-            return JsonSerializer.NonGeneric.Deserialize(messageType, message);
+            return JsonSerializer.NonGeneric.Deserialize(messageType, message, _resolver);
         }
     }
 }
