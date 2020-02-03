49a0617ba53faaa912c1e78e0e8d41ceb091dd1f
Michał Wilczyński <michal@wilczynski.pro>
2020-02-03 23:39:48 +0100

    Rollback to MessagePack 1.9.x, due to serialization issues.

--- src/Outbox/Resonance.Outbox.Serialization.MessagePack/MessagePackSerializer.cs
+++ src/Outbox/Resonance.Outbox.Serialization.MessagePack/MessagePackSerializer.cs
@@ -6,20 +6,29 @@ namespace Resonance.Outbox.Serialization.MessagePack
 {
     public class MessagePackSerializer : IMessageSerializer
     {
-        private readonly MessagePackSerializerOptions _serializerOptions = 
-            MessagePackSerializerOptions.Standard
-                .WithCompression(MessagePackCompression.Lz4Block)
-                .WithAllowAssemblyVersionMismatch(true)
-                .WithResolver(DynamicContractlessObjectResolverAllowPrivate.Instance);
+        public MessagePackSerializer()
+        {
+            CompositeResolver.RegisterAndSetAsDefault(StandardResolverAllowPrivate.Instance,
+                BuiltinResolver.Instance,
+                AttributeFormatterResolver.Instance,
+                // replace enum resolver
+                DynamicEnumAsStringResolver.Instance,
+                DynamicGenericResolver.Instance,
+                DynamicUnionResolver.Instance,
+                DynamicObjectResolver.Instance,
+                PrimitiveObjectResolver.Instance,
+                // final fallback(last priority)
+                StandardResolver.Instance);
+        }
 
         public byte[] Serialize<TMessage>(TMessage message)
         {
-            return global::MessagePack.MessagePackSerializer.Serialize(message, _serializerOptions);
+            return global::MessagePack.MessagePackSerializer.Serialize(message);
         }
 
         public object Deserialize(Type messageType, byte[] message)
         {
-            return global::MessagePack.MessagePackSerializer.Deserialize(messageType, message, _serializerOptions);
+            return global::MessagePack.MessagePackSerializer.NonGeneric.Deserialize(messageType, message);
         }
     }
 }
